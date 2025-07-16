2b24af84a9705dbd86f46ad5b6ad2b7668f2b3a6
Дмитрий Черняев <dcherhtc@gmail.com>
2025-07-16 12:00:51 +0300

    первый коммит

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from langchain_community.vectorstores import FAISS
 import ollama  # pip install ollama
 
 # ------------------------- CONFIG -------------------------
-OLLAMA_MODEL: str = "hf.co/Intelligent-Internet/II-Medical-8B-1706-GGUF:F16"          # сначала «ollama pull <name>»
+OLLAMA_MODEL: str = "hf.co/Qwen/Qwen3-32B-GGUF:Q8_0"          # сначала «ollama pull <name>»
 EMBED_MODEL: str = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 SERVICES_FILE: Path = Path("docs/services.xlsx")
 CHUNK_SIZE: int = 1_000
